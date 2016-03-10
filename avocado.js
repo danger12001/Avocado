1d5cff591a672809cb1d2a04dc1f85d37865b3ac
danger12001 <paul@projectcodex.co>
2016-03-10 16:27:49 +0200

    Almost there...

--- avocado.js
+++ avocado.js
@@ -1,46 +1,50 @@
-var str = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
-convert = function(){
+exports.convert = function(str) {
   var step1 = str.split(", ").join(",").split("for ").join(" ");
   var step2 = step1.split(" ").join("");
   var step3 = step2.split("R").join(",").split(",");
-  console.log(step3);
-  return step3;
+  priceIndex = step3;
+  console.log(priceIndex);
+  return priceIndex;
 };
-var priceIndex = convert();
 
-var quantity = [priceIndex[0], priceIndex[2], priceIndex[4], priceIndex[6]];
-var price = [priceIndex[1], priceIndex[3], priceIndex[5], priceIndex[7]];
-
-var Deal1 = price[0] / quantity[0];
-var d1r = Deal1.toFixed(2);
-var Deal2 = price[1] / quantity[1];
-var d2r = Deal2.toFixed(2);
-var Deal3 = price[2] / quantity[2];
-var d3r = Deal3.toFixed(2);
-var Deal4 = price[3] / quantity[3];
-var d4r = Deal4.toFixed(2);
-
-pricePerAvo = function(){
-  return "the price per avo: " + '\n' + "Deal 1: " + d1r + "\n" + "Deal 2: " + d2r + "\n" + "Deal 3: " + d3r + "\n" + "Deal 4: " + d4r + "\n";
+exports.pricePerAvo = function(priceIndex) {
+  var data = [];
+  var price = [];
+  var quantity = [];
+  for (x = 0; x < priceIndex.length; x++) {
+    if (x % 2 === 0) {
+      var priceVal = parseFloat(priceIndex[x + 1]);
+      data.push({
+        quantity: priceIndex[x],
+        price: priceVal.toFixed(2)
+      });
+    }
+  }
+  for (y = 0; y < data.length; y++) {
+    quantity.push(data[y].quantity);
+    price.push(data[y].price);
+  }
+  console.log(quantity);
+  console.log(price);
+  var singlePrices = [];
+  for (z = 0; z < quantity.length; z++) {
+    singlePrices.push(price[z] / quantity[z]);
+    // singlePrices.toFixed(2);
+  }
+  // parseFloat(singlePrices);
+  console.log('shit i logged', data);
+return singlePrices;
 };
-var singlePrices = [Deal1, Deal2, Deal3, Deal4];
-cheapest = function() {
-  var arr = singlePrices;
-  var min = Math.min.apply(Math, arr).toFixed(2);
+exports.cheapest = function(singlePrices) {
+  var min = Math.min.apply(Math, singlePrices).toFixed(2);
   return "Cheapest deal: " + min;
 };
-expensive = function() {
-  var arr = singlePrices;
-  var max = Math.max.apply(Math, arr).toFixed(2);
+exports.expensive = function(singlePrices) {
+  var max = Math.max.apply(Math, singlePrices).toFixed(2);
   return "Most expensive deal: " + max;
 };
-average = function() {
-  var arr = singlePrices;
-  var average = (singlePrices[0] + singlePrices[1] + singlePrices[2] + singlePrices[3]) / 4;
-  return "The average price is: " + average.toFixed(2);
+exports.average = function(singlePrices) {
+  singlePrices.join(',');
+  var ave = (parseFloat(singlePrices[0]) + parseFloat(singlePrices[1]) + parseFloat(singlePrices[2]) + parseFloat(singlePrices[3])) / 4;
+  return "The average price is: " + ave.toFixed(2);
 };
-module.exports = pricePerAvo;
-module.exports = convert;
-module.exports = cheapest;
-module.exports = expensive;
-module.exports = average;
--- test/avocado_test.js
+++ test/avocado_test.js
@@ -3,31 +3,31 @@ var functions = require('../avocado');
 
 describe("convert",function(){
   it("should convert the string into an array and remove unneeded characters", function(){
-    var result = convert();
+    var result = functions.convert("1 for R3, 2 for R7, 3 for R10, 5 for R14.50");
     assert.deepEqual(result,["1","3","2","7","3","10","5","14.50"]);
   });
 });
 describe("pricePerAvo", function(){
   it("should return the price of one avo in each deal", function(){
-    var result = pricePerAvo();
-    assert.equal(result,"the price per avo: \nDeal 1: 3.00\nDeal 2: 3.50\nDeal 3: 3.33\nDeal 4: 2.90\n");
+    var result = functions.pricePerAvo(priceIndex);
+    assert.deepEqual(result,["3.00","3.50","3.33","2.90"]);
   });
 });
 describe("cheapest", function(){
   it("should return the cheapest deal", function(){
-      var result = cheapest();
+      var result = functions.cheapest([ '3.00', '3.50', '3.33', '2.90' ]);
       assert.equal(result, "Cheapest deal: 2.90");
   });
 });
 describe("expensive", function(){
   it("should return the most expensive deal", function(){
-      var result = expensive();
+      var result = functions.expensive([ '3.00', '3.50', '3.33', '2.90' ]);
       assert.equal(result, "Most expensive deal: 3.50");
   });
 });
 describe("average", function(){
   it("should return the average of the deals", function(){
-      var result = average();
+      var result = functions.average([ '3.00', '3.50', '3.33', '2.90' ]);
       assert.equal(result, "The average price is: 3.18");
   });
 });
