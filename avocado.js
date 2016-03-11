da5fa3ad311bbb3baad20e1dcd30fbd09d60b936
danger12001 <paul@projectcodex.co>
2016-03-11 12:03:12 +0200

    no regex, no global variables and added mapping

--- avocado.js
+++ avocado.js
@@ -2,49 +2,55 @@ exports.convert = function(str) {
   var step1 = str.split(", ").join(",").split("for ").join(" ");
   var step2 = step1.split(" ").join("");
   var step3 = step2.split("R").join(",").split(",");
+console.log(step3);
   priceIndex = step3;
-  console.log(priceIndex);
   return priceIndex;
 };
 
 exports.pricePerAvo = function(priceIndex) {
   var data = [];
-  var price = [];
-  var quantity = [];
   for (x = 0; x < priceIndex.length; x++) {
     if (x % 2 === 0) {
-      var priceVal = parseFloat(priceIndex[x + 1]);
+      var quantityVal = parseFloat(priceIndex[x]).toFixed(2);
+      var priceVal = parseFloat(priceIndex[x + 1]).toFixed(2);
       data.push({
-        quantity: priceIndex[x],
-        price: priceVal.toFixed(2)
+        quantity: Number(quantityVal),
+        price: Number(priceVal)
       });
     }
   }
+  var singlePrices = [];
+  var quantity = [];
+  var price = [];
   for (y = 0; y < data.length; y++) {
+    parseFloat(data.quantity);
+    parseFloat(data.price);
     quantity.push(data[y].quantity);
     price.push(data[y].price);
   }
-  console.log(quantity);
-  console.log(price);
-  var singlePrices = [];
   for (z = 0; z < quantity.length; z++) {
-    singlePrices.push(price[z] / quantity[z]);
-    // singlePrices.toFixed(2);
+    var calc = price[z]/quantity[z];
+    var decimals = calc.toFixed(2);
+    var toNumber = Number(decimals);
+    singlePrices.push(toNumber);
   }
-  // parseFloat(singlePrices);
-  console.log('shit i logged', data);
-return singlePrices;
+  singlePrices.join(",").split(",");
+  sp = singlePrices;
+  return sp;
+
 };
-exports.cheapest = function(singlePrices) {
-  var min = Math.min.apply(Math, singlePrices).toFixed(2);
+exports.cheapest = function(sp) {
+  var min = Math.min.apply(Math, sp ).toFixed(2);
   return "Cheapest deal: " + min;
 };
-exports.expensive = function(singlePrices) {
-  var max = Math.max.apply(Math, singlePrices).toFixed(2);
+exports.expensive = function(sp) {
+  var max = Math.max.apply(Math, sp).toFixed(2);
   return "Most expensive deal: " + max;
 };
-exports.average = function(singlePrices) {
-  singlePrices.join(',');
-  var ave = (parseFloat(singlePrices[0]) + parseFloat(singlePrices[1]) + parseFloat(singlePrices[2]) + parseFloat(singlePrices[3])) / 4;
-  return "The average price is: " + ave.toFixed(2);
+exports.average = function(sp) {
+  for(x = 0;x<sp.length;x++){
+    var sum = sp.reduce(function(a,b) {return a+b;});
+    var ave = sum / sp.length;
+    return "The average price is: " + ave.toFixed(2);
+  }
 };
--- test/avocado_test.js
+++ test/avocado_test.js
@@ -10,24 +10,24 @@ describe("convert",function(){
 describe("pricePerAvo", function(){
   it("should return the price of one avo in each deal", function(){
     var result = functions.pricePerAvo(priceIndex);
-    assert.deepEqual(result,["3.00","3.50","3.33","2.90"]);
+    assert.deepEqual(result,[3,3.50,3.33,2.9]);
   });
 });
 describe("cheapest", function(){
   it("should return the cheapest deal", function(){
-      var result = functions.cheapest([ '3.00', '3.50', '3.33', '2.90' ]);
+      var result = functions.cheapest(sp);
       assert.equal(result, "Cheapest deal: 2.90");
   });
 });
 describe("expensive", function(){
   it("should return the most expensive deal", function(){
-      var result = functions.expensive([ '3.00', '3.50', '3.33', '2.90' ]);
+      var result = functions.expensive(sp);
       assert.equal(result, "Most expensive deal: 3.50");
   });
 });
 describe("average", function(){
   it("should return the average of the deals", function(){
-      var result = functions.average([ '3.00', '3.50', '3.33', '2.90' ]);
+      var result = functions.average(sp);
       assert.equal(result, "The average price is: 3.18");
   });
 });
